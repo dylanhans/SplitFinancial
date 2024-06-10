d77b683123ede2ca9e4ba2f6d74de5fb004d2b50
dylanhans <80360273+dylanhans@users.noreply.github.com>
2024-06-10 15:23:22 -0400

    create intro components

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance}: TotalBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+    <div className="total-balance-chart">
+        /* donut chart*/
+
+
+    </div>
+
+    </section>
+  )
+}
+
+export default TotalBalanceBox
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        'gray-25': '#f9fafb',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
