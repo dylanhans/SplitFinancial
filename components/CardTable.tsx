660097215edc7e529ba8b8232b415cb848d2c70f
dylanhans <80360273+dylanhans@users.noreply.github.com>
2024-07-08 19:20:51 -0400

    cardtable small tweaks

--- components/CardTable.tsx
+++ components/CardTable.tsx
@@ -5,8 +5,8 @@ const CardTable = () => {
   return (
     <div className="w-full rounded-lg">
       {/* First Row with 4 columns */}
-      <ResizablePanelGroup direction="horizontal" className="flex w-full bg-[#f3f4f5] p-1.5 border-t border-b border-gray-200" style={{ height: '135px' }}>
-        <ResizablePanel defaultSize={10} style={{ height: '100%' }}>
+      <ResizablePanelGroup direction="horizontal" className="flex w-full bg-[#f3f4f5] p-1.5 border-t border-b border-gray-200" style={{ height: '130px' }}>
+        <ResizablePanel defaultSize={10} style={{ height: '100%'}}>
           <div className="flex h-full w-[160px] items-center justify-center ml-3">
             <div className="shadow-lg hover:shadow-lg">
               <Image 
@@ -21,29 +21,30 @@ const CardTable = () => {
         <ResizableHandle />
 
 
-        <ResizablePanel defaultSize={10} style={{ height: '100%' }}>
+        <ResizablePanel defaultSize={10} style={{ height: '100%'}}>
           <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
             {/* First Row */}
-            <div className="flex h-full items-center justify-center p-6">
+            <div className="flex h-full items-center justify-center p-6" style={{ height: '100%', boxShadow: '0 0 2px rgba(0, 0, 0, 0.1)'}}>
               <span className="font-semibold">Current Balance</span>
             </div>
 
             {/* Second Row (if needed) */}
-            <div className="flex h-full items-center justify-center p-6 border-t border-gray-200">
+            <div className="flex h-full items-center justify-center p-2 ml-3 mr-auto border-t border-gray-200">
               {/* Content for the second row */}
+              <span className="font-semibold">Credit Limit</span>
             </div>
           </div>
         </ResizablePanel>
         <ResizableHandle />
-        <ResizablePanel defaultSize={10} style={{ height: '100%' }}>
+        <ResizablePanel defaultSize={10} style={{ height: '100%'}}>
           <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
             {/* First Row */}
-            <div className="flex h-full items-center justify-center p-6">
+            <div className="flex h-full items-center justify-center p-6 border-l" style={{ height: '100%', boxShadow: '0 0 2px rgba(0, 0, 0, 0.1)'}}>
               <span className="font-semibold">Available Credit</span>
             </div>
 
             {/* Second Row (if needed) */}
-            <div className="flex h-full items-center justify-center p-6 border-t border-gray-200">
+            <div className="flex h-full items-center justify-center p-2 border-t border-gray-200">
               {/* Content for the second row */}
             </div>
           </div>
@@ -52,7 +53,7 @@ const CardTable = () => {
 
 
 
-        <ResizablePanel defaultSize={10} style={{ height: '100%' }} className="border-l border-gray-200">
+        <ResizablePanel defaultSize={10} style={{ height: '100%'}} className="border-l border-gray-200">
           <div className="flex h-full items-center justify-center p-6">
             <span className="font-semibold">Last Payment</span>
           </div>
@@ -65,13 +66,13 @@ const CardTable = () => {
       
       {/* Third Row with 2 columns */}
       <ResizablePanelGroup direction="horizontal" className="flex w-full mt-3 bg-white">
-        <ResizablePanel defaultSize={15}>
+        <ResizablePanel defaultSize={15} style={{ height: '100%', boxShadow: '0 0 2px rgba(0, 0, 0, 0.1)' }}>
           <div className="flex h-[150px] items-center justify-center p-6">
             <span className="font-semibold">Credit Score</span>
           </div>
         </ResizablePanel>
         <ResizableHandle />
-        <ResizablePanel defaultSize={15} className="custom-border-left border-gray-200">
+        <ResizablePanel defaultSize={15} className="custom-border-left border-gray-200" style={{ height: '100%', boxShadow: '0 0 2px rgba(0, 0, 0, 0.1)' }}>
           <div className="flex h-[150px] items-center justify-center p-6">
             <span className="font-semibold">Statements</span>
           </div>
