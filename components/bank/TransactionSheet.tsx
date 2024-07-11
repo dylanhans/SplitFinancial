d86d215734e381a4e95a1b716e8d4bc99f08125d
dylanhans <80360273+dylanhans@users.noreply.github.com>
2024-07-11 03:03:03 -0400

    sheet update

--- components/bank/TransactionSheet.tsx
+++ components/bank/TransactionSheet.tsx
@@ -8,22 +8,92 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from '@/components/ui/resizable';
 
 const TransactionSheet = ({ isOpen, onClose }) => {
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
-        <SheetClose asChild>
-          <button onClick={onClose}>Close</button>
-        </SheetClose>
-      </SheetContent>
+      <ResizablePanelGroup direction="vertical" className="">
+        <SheetContent className="bg-[#f5f6f7] h-screen w-full">
+          <div className= "clicktrans-nameid bg-white">
+            <SheetHeader className=" w-full border-b border-gray-200 ml-6 p-4">
+              Name of Transaction
+            </SheetHeader>
+            <SheetHeader className=" w-full border-b border-gray-200 ml-6 p-4">
+              Amount
+            </SheetHeader>
+            
+          </div>
+              <div className="transaction-details">
+            <SheetDescription className="bg-[#f5f6f7] w-full border-gray-200 p-10">Transaction Details
+            </SheetDescription>
+              </div>
+            
+            <div className="bg-white transaction-details" style={{ boxShadow: '0px -2px 2px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1)'}}>
+              <div className="p-6">
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Transaction Date
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Posted Date
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Payment Method
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Term
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Every
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Interest
+              </SheetDescription>
+              </div>
+            </div>
+            <div className="bg-white transaction-details">
+              <SheetDescription className="bg-[#f5f6f7] w-full border-gray-200 p-10">
+                    Merchant Details
+              </SheetDescription>
+            </div>
+
+            <div className="bg-white transaction-details" style={{ boxShadow: '0px -2px 2px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1)'}}>
+              <div className="p-6">
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Name
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Location
+              </SheetDescription>
+              </div>
+            </div>
+
+            <div className="bg-white transaction-details">
+              <SheetDescription className="bg-[#f5f6f7] w-full border-b border-gray-200 p-10">
+                    Resources & Help
+              </SheetDescription>
+            </div>
+
+            <div className="bg-white transaction-details" style={{ boxShadow: '0px -2px 2px rgba(0, 0, 0, 0.1)'}}>
+              <div className="p-6">
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Tips
+              </SheetDescription>
+              <SheetDescription className="bg-white w-full border-b border-gray-200 p-2">
+                    Dispute
+              </SheetDescription>
+              </div>
+            </div>
+            <div className="bg-white transaction-details h-full mb-0">
+              <SheetDescription className="bg-[#f5f6f7] w-full border-gray-200 p-10">
+                    
+              </SheetDescription>
+            </div>
+          <SheetClose asChild>
+            <button onClick={onClose}></button>
+          </SheetClose>
+        </SheetContent>
+      </ResizablePanelGroup>
     </Sheet>
   );
 };
