cb8b9c28c3d98ba5fdc537dd0feb81eadd63cd04
dylanhans <80360273+dylanhans@users.noreply.github.com>
2024-09-04 17:11:31 -0400

    removed page

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/page.tsx (or app/page.js)
-import { redirect } from 'next/navigation';
-
-export default function HomePage() {
-  redirect('/home');
-  return null; // This component does not need to render anything
-}
